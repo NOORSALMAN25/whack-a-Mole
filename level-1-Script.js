89e77d1f27fe74c6f27c43aea776a7f79351d206
NOORSALMAN25 <041noorsalman@gmail.com>
2025-07-02 14:59:39 +0300

    create the timer

--- level-1-Script.js
+++ level-1-Script.js
@@ -5,17 +5,36 @@ const replayButton = document.querySelector('.replay')
 const hole = document.querySelector('.holes')
 const score = document.querySelector('.score h3')
 
+const timer = document.querySelector('.timer h3')
+
 // Global variables
 const targetScore = 10
+let target = false
 let currentScore = 0
 let gameActive = false
 let isChange = false
+let startTimer = false
+
+//time count
+const timeCount = () => {
+  if ((startTimer = true)) {
+    for (let i = 20; i >= 0; i--) {
+      setTimeout(() => {
+        timer.innerText = i === 0 || target ? ' ⌛ Done!' : `⌛ ${i} `
+      }, (20 - i) * 1000)
+    }
+  }
+}
 
 // functions
 
-const playTheGame = () => {
-  if ((gameActive = true)) {
-    score.innerText = `SCORE : ${currentScore}`
+const endGame = () => {}
+
+const playTheGame = (Event) => {
+  if ((gameActive = true) && (startTimer = true)) {
+    score.innerText = `⭐ SCORE : ${currentScore}`
+    timer.innerText = `⌛`
+    timeCount()
     moleAppearing()
     hole.addEventListener('click', () => {
       if (isChange) {
@@ -37,19 +56,27 @@ const moleAppearing = () => {
         }, 1000) // 1 second
       }
     }
-  }, 4000)
+  }, 3000)
 }
 
 const scoreCalculator = () => {
   currentScore += 2
-  score.innerText = `SCORE : ${currentScore}`
+  score.innerText = `⭐ SCORE : ${currentScore}`
   if (currentScore >= 10) {
+    target = true
+    startTimer = false
     gameActive = false
   }
 }
+
+const replayFunction = () => {}
 
 // Event Listeners Here
 
 playButton.addEventListener('click', () => {
   playTheGame()
+})
+
+replayButton.addEventListener('click', () => {
+  console.log('replay')
 })
