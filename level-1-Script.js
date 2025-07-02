7d21b8ab8cc90f65b071524a0c0ade21984fcfd0
NOORSALMAN25 <041noorsalman@gmail.com>
2025-07-02 09:03:52 +0300

    add the setInterval for appearing

--- level-1-Script.js
+++ level-1-Script.js
@@ -1,25 +1,35 @@
-// Global variables
+// Cached elements references
 const playButton = document.querySelector('.play')
 const replayButton = document.querySelector('.replay')
 
 const hole = document.querySelector('.holes')
 const score = document.querySelector('.score h3')
+
+// Global variables
 const targetScore = 10
-gameActive = false
+let gameActive = false
+let isChange = false
 
 // functions
 
 const playTheGame = () => {
   gameActive = true
+  moleAppearing()
 }
 
 const moleAppearing = () => {
-  hole.style.backgroundColor = 'pink'
+  setInterval(() => {
+    if (!isChange) {
+      hole.style.backgroundColor = 'pink'
+      isChange = true
+      setTimeout(() => {
+        hole.style.backgroundColor = ''
+        isChange = false
+      }, 2000) // 2 seconds
+    }
+  }, 4000)
 }
 
 const scoreCalculator = () => {}
 
 // testing
-hole.addEventListener('click', () => {
-  moleAppearing()
-})
