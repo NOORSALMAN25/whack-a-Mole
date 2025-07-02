44969c23b5ee1f0260fe1a551df5d5ddadeb97dc
NOORSALMAN25 <041noorsalman@gmail.com>
2025-07-02 10:41:20 +0300

    the score function with interval work

--- level-1-Script.js
+++ level-1-Script.js
@@ -7,29 +7,49 @@ const score = document.querySelector('.score h3')
 
 // Global variables
 const targetScore = 10
+let currentScore = 0
 let gameActive = false
 let isChange = false
 
 // functions
 
 const playTheGame = () => {
-  gameActive = true
-  moleAppearing()
+  if ((gameActive = true)) {
+    score.innerText = `SCORE : ${currentScore}`
+    moleAppearing()
+    hole.addEventListener('click', () => {
+      if (isChange) {
+        scoreCalculator()
+      }
+    })
+  }
 }
 
 const moleAppearing = () => {
   setInterval(() => {
-    if (!isChange) {
-      hole.style.backgroundColor = 'pink'
-      isChange = true
-      setTimeout(() => {
-        hole.style.backgroundColor = ''
-        isChange = false
-      }, 2000) // 2 seconds
+    if (gameActive === true) {
+      if (!isChange) {
+        hole.style.backgroundColor = 'pink'
+        isChange = true
+        setTimeout(() => {
+          hole.style.backgroundColor = ''
+          isChange = false
+        }, 1000) // 1 second
+      }
     }
   }, 4000)
 }
 
-const scoreCalculator = () => {}
+const scoreCalculator = () => {
+  currentScore += 2
+  score.innerText = `SCORE : ${currentScore}`
+  if (currentScore >= 10) {
+    gameActive = false
+  }
+}
 
-// testing
+// Event Listeners Here
+
+playButton.addEventListener('click', () => {
+  playTheGame()
+})
